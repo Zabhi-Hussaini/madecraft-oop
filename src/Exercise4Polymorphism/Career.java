8635eb67bab00655becc048a5b095aa467106c43
Zabhi Husssaini <zabhi.hussaini@sainsburys.co.uk>
2024-08-09 16:33:19 +0100

    OOP Exercises

--- src/Exercise4Polymorphism/Career.java
+++ src/Exercise4Polymorphism/Career.java
@@ -15,6 +15,7 @@ public class Career {
         List<Doctor> doctors = List.of(doctor, gp, surgeon);
         for (Doctor doctor1 : doctors) {
             doctor1.perform();
+            System.out.println();
         }
     }
 }
